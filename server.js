296774f26783a8cd6dafc4d496b4292086396903
Himanshu Garg <himanshu8655@gmail.com>
2024-11-18 01:44:27 -0500

    forgot password API impl

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ import jwt from "jsonwebtoken";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-import { login, signup } from "./db/auth.js";
+import {
+  findPasswordResetToken,
+  login,
+  markResetTokenAsUsed,
+  saveForgotPasswordToken,
+  signup,
+  updateUserPassword,
+  userExists,
+} from "./db/auth.js";
+import nodemailer from "nodemailer";
+import crypto from "crypto";
+import bcrypt from "bcryptjs";
 
 dotenv.config();
 
@@ -123,6 +134,70 @@ app.post("/signup", async (req, res) => {
     return res.status(200).json(jwtResponse);
   } catch (error) {
     return res.status(500).json({ message: "An error occurred during signup" });
+  }
+});
+
+app.post("/forgot-password", async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ message: "Email is required" });
+  }
+
+  try {
+    const user = await userExists(email);
+    if (!user) {
+      return res.status(404).json({ message: "Email ID not found" });
+    }
+
+    const uuid = crypto.randomUUID();
+
+    await saveForgotPasswordToken(email, uuid);
+
+    return res
+      .status(200)
+      .json({ message: "Password reset email sent successfully." });
+  } catch (error) {
+    console.error("Forgot password error:", error.message);
+
+    return res
+      .status(500)
+      .json({ message: "An error occurred while processing your request." });
+  }
+});
+
+app.post("/new-password", async (req, res) => {
+  const { token, password } = req.body;
+
+  if (!token || !password) {
+    return res.status(400).json({ error: "Token and password are required." });
+  }
+
+  try {
+    const resetToken = await findPasswordResetToken(token);
+
+    if (!resetToken) {
+      return res.status(400).json({ error: "Invalid or expired token." });
+    }
+
+    if (resetToken.expiry < Date.now()) {
+      return res.status(400).json({ error: "Token has expired." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+      const isPasswordUpdated = await updateUserPassword(resetToken.email, hashedPassword);
+      console.log(isPasswordUpdated)
+    if (!isPasswordUpdated) {
+      return res.status(400).json({ error: "Failed to update password." });
+    }
+
+    await markResetTokenAsUsed(token);
+
+    res.status(200).json({ message: "Password updated successfully." });
+  } catch (error) {
+    console.error("Error processing new password:", error.message);
+    res.status(500).json({ error: "An error occurred. Please try again." });
   }
 });
 
